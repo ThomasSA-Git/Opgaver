c950a8d9c46e96de71bf372b2f9a0c89dd4b2bb2
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-02-25 13:22:29 +0100

    Last commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,24 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
+    Scanner sc = new Scanner(System.in);
     public void testNavn(){
         System.out.println("Hvad er dit navn?");
-        Scanner sc = new Scanner(System.in);
+
         String navn = sc.next();
         System.out.println("Hej " + navn);
 
     }
 
+    public void askAboutAge(){
+        System.out.println("Hvor gammel er du?");
+        int alder = sc.nextInt();
+        System.out.println(alder);
+    }
+
     public static void main(String[] args) {
         Main obj = new Main();
         obj.testNavn();
+        obj.askAboutAge();
         System.out.println("It works");    }
 }
