3066794fb2dccca60e41980d92744e73f67d3959
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-02-25 13:17:35 +0100

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public void testNavn(){
+        System.out.println("Hvad er dit navn?");
+        Scanner sc = new Scanner(System.in);
+        String navn = sc.next();
+        System.out.println("Hej " + navn);
+
+    }
+
     public static void main(String[] args) {
+        Main obj = new Main();
+        obj.testNavn();
         System.out.println("It works");    }
 }
