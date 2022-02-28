516715dc90951ec992b0c1beb2946bac1c6a70ec
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-02-28 14:27:31 +0100

    gitTest - ændret navn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,20 +7,20 @@ public class Main {
 
   public void testNavn() {
     System.out.println("Hvad er dit navn?");
-
     String navn = sc.next();
     System.out.println("Hej " + navn);
   }
 
-    public void askAboutAge(){
-        System.out.println("Hvor gammel er du?");
-        int alder = sc.nextInt();
-        System.out.println(alder);
-    }
+  public void askAboutAge() {
+    System.out.println("Hvor gammel er du?");
+    int alder = sc.nextInt();
+    System.out.println(alder);
+  }
 
-    public static void main(String[] args) {
-        Main obj = new Main();
-        obj.testNavn();
-        obj.askAboutAge();
-        System.out.println("It works");    }
+  public static void main(String[] args) {
+    Main obj = new Main();
+    obj.testNavn();
+    obj.askAboutAge();
+    System.out.println("It works");
+  }
 }
